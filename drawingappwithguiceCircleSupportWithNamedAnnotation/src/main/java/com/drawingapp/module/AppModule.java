71a9f165496861e17ab925de505dec1a6fc45966
Malcolm Boekhoff <Malcolm.Boekhoff@fincleartech.com.au>
2025-10-08 11:35:16 +1100

    main-Malcolm.Boekhoff-20251008113514 Replace user annotations with Java Named ones

--- drawingappwithguiceCircleSupportWithNamedAnnotation/src/main/java/com/drawingapp/module/AppModule.java
+++ drawingappwithguiceCircleSupportWithNamedAnnotation/src/main/java/com/drawingapp/module/AppModule.java
@@ -1,20 +1,17 @@
 package com.drawingapp.module;
 
-import com.drawingapp.annotations.Circle;
-import com.drawingapp.annotations.Square;
 import com.drawingapp.services.DrawCircle;
 import com.drawingapp.services.DrawShape;
 import com.drawingapp.services.DrawSquare;
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
 
+public class AppModule extends AbstractModule {
 
-public class AppModule extends AbstractModule{
-	
-	@Override
-	protected void configure() {
-		bind(DrawShape.class).annotatedWith(Names.named("Square")).to(DrawSquare.class);
-		bind(DrawShape.class).annotatedWith(Names.named("Circle1")).to(DrawCircle.class);
-	}
-	
+    @Override
+    protected void configure() {
+        bind(DrawShape.class).annotatedWith(Names.named("Square")).to(DrawSquare.class);
+        bind(DrawShape.class).annotatedWith(Names.named("Circle")).to(DrawCircle.class);
+    }
+
 }
--- drawingappwithguiceCircleSupportWithNamedAnnotation/src/main/java/com/drawingapp/requests/SquareRequest.java
+++ drawingappwithguiceCircleSupportWithNamedAnnotation/src/main/java/com/drawingapp/requests/SquareRequest.java
@@ -3,17 +3,17 @@ package com.drawingapp.requests;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import com.drawingapp.annotations.Square;
 import com.drawingapp.services.DrawShape;
 
-public class SquareRequest {	
-	DrawShape d;
-	
-	@Inject
-	public SquareRequest(@Named("Square") DrawShape d){
-		this.d = d;
-	}
-	public void makeRequest(){
-		d.draw();
-	}	
+public class SquareRequest {
+    DrawShape d;
+
+    @Inject
+    public SquareRequest(@Named("Square") DrawShape d) {
+        this.d = d;
+    }
+
+    public void makeRequest() {
+        d.draw();
+    }
 }
