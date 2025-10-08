2c56fe0ffc4b2ea66e1c65382016a86c886ed44e
Malcolm Boekhoff <Malcolm.Boekhoff@fincleartech.com.au>
2025-10-08 13:14:26 +1100

    main-Malcolm.Boekhoff-20251008131425 Annotating provides method for circle

--- drawingappwithguiceProvidesWithSqaureAndCircleSupport/src/main/java/com/drawingapp/main/GuiceDemo.java
+++ drawingappwithguiceProvidesWithSqaureAndCircleSupport/src/main/java/com/drawingapp/main/GuiceDemo.java
@@ -7,28 +7,28 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class GuiceDemo {
-	
-	private static final String SQAURE_REQ = "SQUARE";
-	private static final String CIRCLE_REQ = "CIRCLE";	
-	
-	private static void sendRequest(String sqaureReq) {
-		
-		if(sqaureReq.equals(SQAURE_REQ)){			
-			Injector injector = Guice.createInjector(new AppModule());			
-			SquareRequest request = injector.getInstance(SquareRequest.class);
-			request.makeRequest();
-		}
-		
-		if(sqaureReq.equals(CIRCLE_REQ)){			
-			Injector injector = Guice.createInjector(new AppModule());			
-			CircleRequest request = injector.getInstance(CircleRequest.class);
-			request.makeRequest();
-		}	
-		
-	}
-	
-	public static void main(String[] args) {		
-		//sendRequest(SQAURE_REQ);
-		sendRequest(CIRCLE_REQ);
-	}
+
+    private static final String SQAURE_REQ = "SQUARE";
+    private static final String CIRCLE_REQ = "CIRCLE";
+
+    private static void sendRequest(String sqaureReq) {
+
+        if (sqaureReq.equals(SQAURE_REQ)) {
+            Injector injector = Guice.createInjector(new AppModule());
+            SquareRequest request = injector.getInstance(SquareRequest.class);
+            request.makeRequest();
+        }
+
+        if (sqaureReq.equals(CIRCLE_REQ)) {
+            Injector injector = Guice.createInjector(new AppModule());
+            CircleRequest request = injector.getInstance(CircleRequest.class);
+            request.makeRequest();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        sendRequest(SQAURE_REQ);
+        sendRequest(CIRCLE_REQ);
+    }
 }
--- drawingappwithguiceProvidesWithSqaureAndCircleSupport/src/main/java/com/drawingapp/requests/CircleRequest.java
+++ drawingappwithguiceProvidesWithSqaureAndCircleSupport/src/main/java/com/drawingapp/requests/CircleRequest.java
@@ -3,17 +3,17 @@ package com.drawingapp.requests;
 import javax.inject.Inject;
 
 import com.drawingapp.annotations.Circle;
-import com.drawingapp.annotations.Square;
 import com.drawingapp.services.DrawShape;
 
-public class CircleRequest {	
-	DrawShape d;
-	
-	@Inject
-	public CircleRequest(@Circle DrawShape d){
-		this.d = d;
-	}
-	public void makeRequest(){
-		d.draw();
-	}	
+public class CircleRequest {
+    DrawShape d;
+
+    @Inject
+    public CircleRequest(@Circle DrawShape d) {
+        this.d = d;
+    }
+
+    public void makeRequest() {
+        d.draw();
+    }
 }
